f4113632a281c8591b8abc91cdac25e9392331af
0820543660 <pedro.baptista@serpro.gov.br>
2020-05-21 09:45:06 -0300

    Start

---
+++ material_app/lib/Widgets/Pages/home_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello Flutter"),
+        centerTitle: true,
+      ),
+      body: _body(context),
+    );
+  }
+
+  _body(context) {
+    return Container(
+      color: Colors.red,
+
+      child: SizedBox.expand(
+        child: Column(
+
+          children: _columnRow(context),
+        ),
+      )
+    );
+  }
+
+  _columnRow(context) {
+    return <Widget>[
+      Container(
+        color: Colors.yellow,
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            _button(),
+            _button(),
+            _button(),
+          ],
+        ),
+      ),
+      Container(
+        color: Colors.yellow,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            _button(),
+            _button(),
+            _button(),
+          ],
+        ),
+      ),
+    ];
+  }
+
+  _button() {
+//    return Container(
+//      color: Colors.white,
+//      child: Center(
+//        child: _button(),
+//      ),
+//    );
+    return RaisedButton(
+      color: Colors.blue,
+      child: Text(
+        'Ok',
+        style: TextStyle(color: Colors.white, fontSize: 30),
+      ),
+      onPressed: () => _onOkPressed("Parametro"),
+    );
+  }
+
+  _onOkPressed(param) {
+    print(param);
+  }
+
+  _img() {
+//    return Container(
+//      color: Colors.white,
+////      child: ListView(
+////          children: <Widget>[
+////            _text(),
+////            _img()
+////          ]
+////      ),
+////      child: SizedBox.expand(
+////          child: _img()
+////      ),
+//      child: Center(
+//          child: _img()
+//      ),
+//      //margin: EdgeInsets.only(left:10, top: 10)
+//    );
+
+    //return Image.network('https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQic4Tx27KieTfowQBGUET4h8LKvrdHfU1UZ6qY_9B3pU0_uMVG&usqp=CAU');
+    return Image.asset('assets/images/dog1.jpg', fit: BoxFit.cover);
+  }
+
+  _text() {
+//    return Container(
+//       color: Colors.white,
+//       child: _text()
+//      ),
+//      margin: EdgeInsets.only(left:10, top: 10)
+//    );
+
+    return Text("Hello World!",
+        style: TextStyle(
+            fontSize: 30,
+            color: Colors.blue,
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+            decoration: TextDecoration.underline,
+            decorationColor: Colors.red,
+            decorationStyle: TextDecorationStyle.wavy));
+  }
+}
